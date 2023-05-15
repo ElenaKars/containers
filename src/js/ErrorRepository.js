d7ffb9dc47e2f1fd12f7e8ba5a7948335510fc91
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2023-05-15 16:05:46 +0800

    updated ErrorRepository.js

--- src/js/ErrorRepository.js
+++ src/js/ErrorRepository.js
@@ -1,6 +1,10 @@
 export default class ErrorRepository {
   constructor(){
     this.errors = new Map();
+
+    this.addError(1, 'Error 1');
+    this.addError(2, 'Error 2');
+    this.addError(3, 'Error 3');
   }
   addError(code, message){
     this.errors.set(code, message);
